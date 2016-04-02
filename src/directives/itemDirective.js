b89a125ca82319ee3a5a6da2c8e091d8c01cd8e3
AmenallahMeftehh <meftah.amenallah@gmail.com>
2016-04-02 16:26:56 +0100

    1.0 CV

--- src/directives/itemDirective.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-app
-.directive('cvDirective',function(){
-return{
-template:'{{titrescv.Nom}} a obtenu {{titrescv.Diplomes}} .Ses Experiences professionelles : {{titrescv.Experiences Professionelles}} /n Ses compétances sont:{{titrescv.Compétances}}'
-}
-})
